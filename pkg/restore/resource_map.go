5c1e65809f79bd34045e03806c95c6a90cb5f2ef
Brian Chen <brian.chen20@det.nsw.edu.au>
2023-09-24 17:10:06 +1000

    query Schema.GroupVersionResource by group.kind string

--- pkg/restore/resource_map.go
+++ pkg/restore/resource_map.go
@@ -1,6 +1,13 @@
 package restore
 
-import "k8s.io/apimachinery/pkg/runtime/schema"
+import (
+	"fmt"
+	"github.com/pkg/errors"
+	"k8s.io/apimachinery/pkg/runtime/schema"
+	"k8s.io/client-go/discovery"
+	"k8s.io/client-go/rest"
+	"k8s.io/client-go/restmapper"
+)
 
 var kindResourceMap = map[string]schema.GroupVersionResource{
 	"Deployment": {
@@ -43,4 +50,20 @@ var kindResourceMap = map[string]schema.GroupVersionResource{
 		Version:  "v1beta1",
 		Resource: "customresourcedefinitions",
 	},
+}
+
+// GetGroupVersionResource (gk string), gk string group and kind as: "apps.Deployment"
+func GetGroupVersionResource(gk string) (*schema.GroupVersionResource, error) {
+	c := discovery.NewDiscoveryClientForConfigOrDie(&rest.Config{})
+
+	groupResources, err := restmapper.GetAPIGroupResources(c)
+	if err != nil {
+		return nil, errors.Wrap(err, "getting Dynamic client")
+	}
+	mapper := restmapper.NewDiscoveryRESTMapper(groupResources)
+
+	mapping, err := mapper.RESTMapping(schema.ParseGroupKind("apps.Deployment"))
+	fmt.Println(mapping.Resource)
+
+	return &mapping.Resource, nil
 }
